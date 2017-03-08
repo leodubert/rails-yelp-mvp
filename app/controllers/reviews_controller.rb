8dac609f06e8cb2517af9e918bc9fcd6e3dad5c6
Leo Dubert <leodubert@gmail.com>
2017-03-08 16:34:54 +0100

    with form optimisation

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,9 +15,8 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-      if @review.save
+    if @review.save
       redirect_to restaurant_path(@restaurant)
-
     else
       render :new
     end
